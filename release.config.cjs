641fc9bd0dda9ec79d1fe5957cb24575c602302f
Phil Gee <originalphil@gmail.com>
2026-03-04 16:29:46 +0000

    AEA-5986 Conditionally add pypi plugin.

--- release.config.cjs
+++ release.config.cjs
@@ -8,100 +8,106 @@ const mainBranch = process.env.MAIN_BRANCH || "main"
 const pypiPublish = process.env.PYPI_PUBLISH?.toLowerCase() === 'true' || false
 const pypiToken = process.env.PYPI_TOKEN
 
+const pypiPlugin = [
+    "semantic-release-pypi",
+    {
+        repoToken: pypiToken
+    }
+]
+
+const plugins = [
+    [
+        "@semantic-release/commit-analyzer",
+        {
+            preset: "eslint",
+            releaseRules: [
+                {
+                    tag: "Fix",
+                    release: "patch"
+                },
+                {
+                    tag: "Update",
+                    release: "patch"
+                },
+                {
+                    tag: "New",
+                    release: "minor"
+                },
+                {
+                    tag: "Breaking",
+                    release: "major"
+                },
+                {
+                    tag: "Docs",
+                    release: "patch"
+                },
+                {
+                    tag: "Build",
+                    release: false
+                },
+                {
+                    tag: "Upgrade",
+                    release: "patch"
+                },
+                {
+                    tag: "Chore",
+                    release: "patch"
+                }
+            ]
+        }
+    ],
+    [
+        "@semantic-release/release-notes-generator",
+        {
+            preset: "eslint",
+            writerOpts: {
+                commitPartial: commitTemplate
+            }
+        }
+    ],
+    [
+        "@semantic-release/changelog",
+        {
+            changelogFile: "CHANGELOG.md"
+        }
+    ],
+    ...publish_packages.map(subpackage => [
+        "@semantic-release/npm",
+        {
+            pkgRoot: subpackage
+        }
+    ]),
+    [
+        "@semantic-release/github",
+        {
+            assets: [
+                {
+                    path: "CHANGELOG.md",
+                    label: "CHANGELOG.md"
+                },
+                ...(process.env.EXTRA_ASSET ? [
+                    {
+                        path: process.env.EXTRA_ASSET,
+                        label: process.env.EXTRA_ASSET
+                    }
+                ] : [])
+            ],
+            successComment: false,
+            failComment: false,
+            failTitle: false
+        }
+    ]
+]
+
+if (pypiPublish) {
+    plugins.push(pypiPlugin)
+}
+
 module.exports = {
     branches: [
         {
             name: mainBranch
         }
     ],
-    plugins: [
-        [
-            "@semantic-release/commit-analyzer",
-            {
-                preset: "eslint",
-                releaseRules: [
-                    {
-                        tag: "Fix",
-                        release: "patch"
-                    },
-                    {
-                        tag: "Update",
-                        release: "patch"
-                    },
-                    {
-                        tag: "New",
-                        release: "minor"
-                    },
-                    {
-                        tag: "Breaking",
-                        release: "major"
-                    },
-                    {
-                        tag: "Docs",
-                        release: "patch"
-                    },
-                    {
-                        tag: "Build",
-                        release: false
-                    },
-                    {
-                        tag: "Upgrade",
-                        release: "patch"
-                    },
-                    {
-                        tag: "Chore",
-                        release: "patch"
-                    }
-                ]
-            }
-        ],
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                preset: "eslint",
-                writerOpts: {
-                    commitPartial: commitTemplate
-                }
-            }
-        ],
-        [
-            "@semantic-release/changelog",
-            {
-                changelogFile: "CHANGELOG.md"
-            }
-        ],
-        ...publish_packages.map(subpackage => [
-            "@semantic-release/npm",
-            {
-                pkgRoot: subpackage
-            }
-        ]),
-        [
-            "semantic-release-pypi",
-            {
-                pypiPublish: pypiPublish,
-                repoToken: pypiToken
-            }
-        ],
-        [
-            "@semantic-release/github",
-            {
-                assets: [
-                    {
-                        path: "CHANGELOG.md",
-                        label: "CHANGELOG.md"
-                    },
-                    ...(process.env.EXTRA_ASSET ? [
-                        {
-                            path: process.env.EXTRA_ASSET,
-                            label: process.env.EXTRA_ASSET
-                        }
-                    ] : [])
-                ],
-                successComment: false,
-                failComment: false,
-                failTitle: false
-            }
-        ]
-    ]
+    plugins: plugins
 }
